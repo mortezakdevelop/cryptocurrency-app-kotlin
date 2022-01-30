6dbec5c56c926204de6b18f9fc3118d948967c0b
mortezakdevelop <m.vortex3537007@gmail.com>
2022-01-30 14:08:15 -0800

    drawer layout

--- app/src/main/java/com/example/cryptocurrencykotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencykotlinapplication/MainActivity.kt
@@ -1,29 +1,39 @@
 package com.example.cryptocurrencykotlinapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.cryptocurrencykotlinapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     lateinit var navHostFragment: NavHostFragment
-    lateinit var navController:NavController
+    lateinit var navController: NavController
+    lateinit var appbarConfiguration: AppBarConfiguration
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setupNavigationFragments()
 
-     }
-    private fun setupNavigationFragments(){
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+
+    private fun setupNavigationFragments() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        //setup drawerLayout
+        appbarConfiguration =
+            AppBarConfiguration.Builder(R.id.homeFragment, R.id.marketFragment, R.id.charFragment)
+                .setOpenableLayout(binding.drawerLayout).build()
+
         setupSmoothBottomMenu()
     }
 
--- app/src/main/java/com/example/cryptocurrencykotlinapplication/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/cryptocurrencykotlinapplication/fragment/HomeFragment.kt
@@ -1,17 +1,25 @@
 package com.example.cryptocurrencykotlinapplication.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.example.cryptocurrencykotlinapplication.MainActivity
 import com.example.cryptocurrencykotlinapplication.R
 import com.example.cryptocurrencykotlinapplication.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     lateinit var binging:FragmentHomeBinding
+    lateinit var mainActivity:MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
